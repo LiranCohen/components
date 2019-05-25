5e0cb461d35748e9152875ccfe74aeaa7727be86
Liran Cohen <lcohen@ncl.com>
2019-05-25 19:02:22 -0400

    Vue Bootstrap Install

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Signup/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Signup from './components/user/Signup.vue';
 
 export default Vue.extend({
   name: 'app',
   components: {
-    HelloWorld,
+    Signup,
+  },
+  data: () => {
+    return {
+      title: 'Signup Form',
+    };
   },
 });
 </script>
