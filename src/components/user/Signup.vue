e8c49bc3e8a2083f61f8e856b7dd3d6d9403e20a
Liran Cohen <lcohen@ncl.com>
2019-05-25 17:32:47 -0400

    Install StoryBook via VUE UI

---
+++ src/components/user/Signup.vue
@@ -0,0 +1,15 @@
+<template>
+    
+</template>
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class SignUp extends Vue {
+    @Prop(String) readonly title!: string 
+}
+</script>
+<style>
+
+</style>
+
